61118747ae074520f04a3a565ed1bad7421131de
Andy Durdin <me@andy.durdin.net>
2021-04-15 15:48:26 +0100

    skeleton

---
+++ __init__.py
@@ -0,0 +1,61 @@
+bl_info = {
+    'name': 'Thieftools',
+    'author': 'vfig',
+    'version': (0, 0, 1),
+    'blender': (2, 90, 0),
+    'category': '(Development)',
+    'description': '(in development)'
+}
+
+if "bpy" in locals():
+    import importlib as imp
+    imp.reload(mission)
+    print("thieftools: reloaded.");
+else:
+    from . import mission
+    print("thieftools: loaded.");
+
+
+import bpy
+from bpy.props import CollectionProperty
+from bpy.types import Panel
+
+
+#---------------------------------------------------------------------------#
+# Panels
+
+class TOOLS_PT_thieftools_debug(Panel):
+    bl_label = "Thief"
+    bl_idname = "TOOLS_PT_thieftools_debug"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "Mission"
+    bl_context = "objectmode"
+
+    @classmethod
+    def poll(self, context):
+        # This panel should always be available.
+        return True
+
+    def draw(self, context):
+        layout = self.layout
+        row = layout.row(align=True)
+        box = row.box()
+        box.label(text="Foo")
+        row = box.row(align=True)
+        row.operator("object.tt_debug_import_mission", text="Import (debug)")
+
+
+#---------------------------------------------------------------------------#
+# Register and unregister
+
+def register():
+    bpy.utils.register_class(mission.TTDebugImportMissionOperator)
+    bpy.utils.register_class(TOOLS_PT_thieftools_debug)
+    print("thieftools: registered.");
+
+
+def unregister():
+    bpy.utils.unregister_class(TOOLS_PT_thieftools_debug)
+    bpy.utils.unregister_class(mission.DebugImportMissionOperator)
+    print("thieftools: unregistered.");
---
+++ mission.py
@@ -0,0 +1,37 @@
+import bpy
+import bmesh
+import base64
+import math
+import mathutils
+import random
+import struct
+import zlib
+
+from array import array
+from itertools import islice
+from bpy.props import IntProperty, PointerProperty, StringProperty
+from bpy.types import Object, Operator, Panel, PropertyGroup
+from mathutils import Vector
+from mathutils.bvhtree import BVHTree
+
+#---------------------------------------------------------------------------#
+# Operators
+
+class TTDebugImportMissionOperator(Operator):
+    bl_idname = "object.tt_debug_import_mission"
+    bl_label = "Import mission"
+    bl_options = {'REGISTER'}
+
+    filename : StringProperty()
+
+    def execute(self, context):
+        if context.mode != "OBJECT":
+            self.report({'WARNING'}, f"{self.bl_label}: must be in Object mode.")
+            return {'CANCELLED'}
+
+        bpy.ops.object.select_all(action='DESELECT')
+
+        #context.view_layer.objects.active = o
+        #o.select_set(True)
+
+        return {'FINISHED'}
